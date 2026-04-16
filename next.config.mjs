0e09ca63a8f7b30d20114bc25bebe12d0fd4f36f
kareem ibrahem <kareemibrahem2002@gmail.com>
2026-04-16 13:48:26 +0200

    fix build errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
-    i18n: {
-        locales: ['en', 'ar'],
-        defaultLocale: 'en',
-    }
+    // eslint: {
+    //     ignoreDuringBuilds: true,
+    // },
+    // i18n: {
+    //     locales: ['en', 'ar'],
+    //     defaultLocale: 'en',
+    // }
 };
 
 
