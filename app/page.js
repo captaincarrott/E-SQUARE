2b63e0f04f0c265bd2169db5a9260a0133f95843
captaincarrott <you@example.com>
2026-04-21 20:18:27 +0200

    addon

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ export default async function Home() {
       {/* HERO */}
       <section className="bg-[#1C65A2] text-white text-center py-16 px-4">
         <h1 className="text-4xl font-bold mb-4">Welcome to Esquare²</h1>
-        <p className="mb-6">Best products with best prices 🔥</p>
+        <p className="mb-6">Best products with best prices</p>
         <a
           href="/products"
           className="bg-white text-[#1C65A2] px-6 py-2 rounded font-semibold"
